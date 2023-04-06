ff9032193ccb4a4eaa1d3234d4c3f122ee4195a1
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-04-06 02:17:18 +0000

    admin.user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+
+  devise_for :admin,  skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+    # home
+    root to: 'homes#top'
+  end
+
+  scope module: :public do
+
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
 end
