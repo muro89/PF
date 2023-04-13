dd63b99c132e371dfa53389f2f77698eda6e8d55
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-04-13 09:15:40 +0000

    status.add

--- config/routes.rb
+++ config/routes.rb
@@ -18,11 +18,15 @@ Rails.application.routes.draw do
   scope module: :public do
     resources :users, only: [:show, :edit, :update, :index]
     resources :posts
-    
+
     get "search" => "searches#search" , as:'search'
     get "search_tag"=>"posts#search_tag"
     root to: 'homes#top'
     get 'about' => 'homes#about'
+     # 退会確認画面
+    get '/users/:id/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+    # 論理削除用のルーティング
+    patch '/users/:id/withdrawal' => 'users#withdrawal', as: 'withdrawal'
   end
 
 end
