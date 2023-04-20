42ef4aac8a0e41b1e4063e040cfc870411378931
YukiIshimuroya <122260585+muro89@users.noreply.github.com>
2023-04-20 10:54:04 +0000

    tag.edit

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :post_tags,dependent: :destroy,  foreign_key: 'tag_id'
   has_many :posts,through: :post_tags
 
-  validates :names, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true
 end
